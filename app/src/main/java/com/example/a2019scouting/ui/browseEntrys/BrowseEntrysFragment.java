bf5a67559cc02d7f5c2794b232d71f107abc8e69
coyotew1 <55400703+coyotew1@users.noreply.github.com>
2019-10-29 12:28:09 -0600

    can read
    Can now read from file and display in the browse current entrys tab.  However, it just reads the text file and is not parsed correctly.  working on that rn

--- app/src/main/java/com/example/a2019scouting/ui/browseEntrys/BrowseEntrysFragment.java
+++ app/src/main/java/com/example/a2019scouting/ui/browseEntrys/BrowseEntrysFragment.java
@@ -1,6 +1,8 @@
 package com.example.a2019scouting.ui.browseEntrys;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +16,12 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.example.a2019scouting.R;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class BrowseEntrysFragment extends Fragment {
 
     private BrowseEntrysViewModel browseEntrysViewModel;
@@ -23,13 +31,50 @@ public class BrowseEntrysFragment extends Fragment {
         browseEntrysViewModel =
                 ViewModelProviders.of(this).get(BrowseEntrysViewModel.class);
         View root = inflater.inflate(R.layout.fragment_share, container, false);
-        final TextView textView = root.findViewById(R.id.text_share);
+        final TextView mainView = root.findViewById(R.id.MainViewer);
+
+
+        String in = readFromFile();
+
+
+
         browseEntrysViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+                mainView.setText(in);
             }
         });
+
+
+
         return root;
+    }
+    public String readFromFile() {
+        Context context = getContext();
+        String ret = "";
+
+        try {
+            InputStream inputStream = context.openFileInput("ass.txt");
+
+            if ( inputStream != null ) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String receiveString = "";
+                StringBuilder stringBuilder = new StringBuilder();
+
+                while ( (receiveString = bufferedReader.readLine()) != null ) {
+                    stringBuilder.append(receiveString);
+                }
+
+                inputStream.close();
+                ret = stringBuilder.toString();
+            }
+        }
+        catch (FileNotFoundException e) {
+            Log.e("login activity", "File not found: " + e.toString());
+        } catch (IOException e) {
+            Log.e("login activity", "Can not read file: " + e.toString());
+        }
+        return ret;
     }
 }
