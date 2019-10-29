8d566bc27864077776752d297ed65435212e112d
coyotew1 <55400703+coyotew1@users.noreply.github.com>
2019-10-28 18:37:47 -0600

    Added basic UI in xml
    Added ui for basic form, must be expanded upon later

--- app/src/main/java/com/example/a2019scouting/ui/newEntry/NewEntryViewModel.java
+++ app/src/main/java/com/example/a2019scouting/ui/newEntry/NewEntryViewModel.java
@@ -8,20 +8,10 @@ public class NewEntryViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public NewEntryViewModel() {
+    public NewEntryViewModel()
+    {
         mText = new MutableLiveData<>();
-        mText.setValue("ADD ENTRY ACTIVITY HERE" +
-                "SoMETIMES\n" +
-                "\n" +
-                "God TaKes MOMMIES\n" +
-                "\n" +
-                "aNd PuPPIES AWAY…\n" +
-                "\n" +
-                "And SoMETIMES…\n" +
-                "\n" +
-                "Just SoMETIMES…\n" +
-                "\n" +
-                " I do.");
+
     }
 
     public LiveData<String> getText() {
